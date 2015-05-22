8479f0342d3d48df1ee7d0db4eafa96a0d151f43
Quoclap <quoclap.hedspi@gmail.com>
2015-05-23 00:54:55 +0900

    20150523 : hello world

---
+++ HelloCocos/proj.ios_mac/ios/notefile.cpp
@@ -0,0 +1,17 @@
+//
+//  notefile.cpp
+//  HelloCocos
+//
+//  Created by Quoclap on 2015/05/15.
+//
+//
+
+#include "notefile.h"
+
+
+auto animals= {1,2,3,4,5};
+
+for(auto animal : animals){
+    std::count<< amimal<< std::endl;
+    
+}
---
+++ HelloCocos/proj.ios_mac/ios/notefile.h
@@ -0,0 +1,14 @@
+//
+//  notefile.h
+//  HelloCocos
+//
+//  Created by Quoclap on 2015/05/15.
+//
+//
+
+#ifndef __HelloCocos__notefile__
+#define __HelloCocos__notefile__
+
+#include <stdio.h>
+
+#endif /* defined(__HelloCocos__notefile__) */
